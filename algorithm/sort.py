058fc656763de33015a48d5be23f7cd2a14d2854
wanglin <wanglin20130410@126.com>
2017-01-15 13:11:32 +0800

    insert whitespace after ,

--- algorithm/sort.py
+++ algorithm/sort.py
@@ -39,12 +39,12 @@ def quick_sort(arr, low, high):
 
 def bubble_sort(arr):
     for i in range(len(arr))[::-1]: # from tail to header
-        for j in range(i): # compare current with next from header to i
+        for j in range(i):  # compare current with next from header to i
             if arr[j] > arr[j+1]:
-                arr[j],arr[j+1] = arr[j+1],arr[j]
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
 if __name__ == '__main__':
-    arr = [23,2,243,12,3]
+    arr = [23, 2, 243, 12, 3]
     bubble_sort(arr)
     print arr
